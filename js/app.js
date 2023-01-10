bf6c62583e487696f188583946316242c963ed8d
Rody Huancas <rodyhuancas.04@gmail.com>
2023-01-09 19:04:10 -0500

    Actualización

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 const button = document.querySelector("button");
 const front = document.querySelector(".front");
 const back = document.querySelector(".back");
+const alertMessage = document.querySelector(".alert");
+let inputValue;
 
-button.addEventListener("click", function () {
+document.addEventListener("click", function (e) {
+    if (e.target.className === "input") {
+        inputValue = e.target.value;
+    }
+
+});
+
+button.addEventListener("click", function (e) {
     front.classList.add("active")
     back.classList.remove("active")
-
+    console.log(inputValue);
+    alertMessage.innerHTML = `You selected ${inputValue} out of 5`;
     setTimeout(() => {
         front.classList.remove("active")
         back.classList.add("active")
